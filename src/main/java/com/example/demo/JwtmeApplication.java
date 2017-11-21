83b00a0611277255b333173f24950c95ac3b0bbf
Marvan Boff <marvan.boff@cwi.com.br>
2017-11-21 11:13:40 -0200

    Creating poc using jwt

---
+++ src/main/java/com/example/demo/JwtmeApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class JwtmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtmeApplication.class, args);
+	}
+	
+	@RequestMapping("/home")
+	public String hello() {
+		return "Hello buddy!";
+	}
+
+	@PostMapping("/login")
+	public void teste() {
+		System.out.println("testeando");
+	}
+}
